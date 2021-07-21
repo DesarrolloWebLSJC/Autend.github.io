4748b5d73e63f2b65d98b41f6f775b528e57fba8
DesarrolloWebLSJC <87743303+DesarrolloWebLSJC@users.noreply.github.com>
2021-07-21 00:55:55 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,13 @@
+
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyDo7NGnKRbNhCYlv9CziKH-64UxTil3Tyg",
+    authDomain: "autendlsjc.firebaseapp.com",
+    projectId: "autendlsjc",
+    storageBucket: "autendlsjc.appspot.com",
+    messagingSenderId: "789524433214",
+    appId: "1:789524433214:web:3727625d436f7c26ee046f",
+    measurementId: "G-MSSSN1Z1QY"
+});
